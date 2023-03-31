03b32c757723051c6a3e51ec84c49b2864ed27eb
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2023-03-31 08:34:16 +0500

    Updated

---
+++ Lab6/Components/FoodieInput.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { StyleSheet, View, TextInput, Image } from 'react-native';
+
+export default function FoodieInput(props) {
+  return (
+    <View style={styles.inputContainer}>
+      {props.haveIcon == true && (
+        <Image source={props.path} style={styles.icon} />
+      )}
+      <TextInput
+        style={styles.input}
+        placeholder={props.placeholder}
+        keyboardType={props.keyboardType}
+        underlineColorAndroid="transparent"
+        onChangeText={props.onChangeText}
+        secureTextEntry={props.secureTextEntry}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#FBFCFF',
+    borderRadius: 22,
+    marginBottom: 18,
+  },
+  input: {
+    flex: 1,
+    height: 60,
+    fontSize: 12,
+    justifyContent: 'center',
+    marginStart: 22,
+    color: '#383F53',
+    fontSize: 12,
+  },
+  icon: {
+    marginStart: 22,
+    height: 26,
+    width: 26,
+  },
+});
